b4470b1f03f5f7883efa6f05583f1113d1247f77
ahlemgit-prog <71451312+ahlemgit-prog@users.noreply.github.com>
2023-12-20 15:15:44 +0100

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,58 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    fontFamily: {
+      'body': [' "Livvic" '],
+      guldscript: ['var(--font-guldscript']
+    },
+    fontWeight: {
+      thin: '100',
+      hairline: '100',
+      extralight: '200',
+      light: '300',
+      normal: '400',
+      medium: '500',
+      semibold: '600',
+      bold: '700',
+      extrabold: '800',
+      'extra-bold': '800',
+      black: '900',
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+
+      keyframes: {
+        wiggle: {
+          "0% 100%": {
+            transform: "rotate(-10deg)",
+          },
+          "50%": { transform: "rotate(-10deg)" },
+        },
+      }
     },
   },
   plugins: [],
